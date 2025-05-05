9b3570d9791d4c6e05be78948a81869dde997a3d
DevaprakashP <devaprakashdearson@gmail.com>
2025-05-05 11:01:00 +0530

    updated code

--- src/component/board.js
+++ src/component/board.js
@@ -21,71 +21,59 @@ const SnakeBoard = () => {
         let className = "cell";
 
         let isFood = food.x === row && food.y === col;
-
         let isSnake = snake.some((ele) => ele.x === row && ele.y === col);
-
         let isSnakeHead = snake[0].x === row && snake[0].y === col;
 
-        if (isFood) {
-          className += " food";
-        }
-        if (isSnake) {
-          className += " snake";
-        }
-        if (isSnakeHead) {
-          className += " snakeHead";
-        }
+        if (isFood) className += " food";
+        if (isSnake) className += " snake";
+        if (isSnakeHead) className += " snakeHead";
 
-        const cell = <div className={className} key={`${row}-${col}`}></div>;
-        cellArray.push(cell);
+        cellArray.push(<div className={className} key={`${row}-${col}`}></div>);
       }
     }
     return cellArray;
   }
-  const cells = Board();
 
-  function gameOver() {
+  function resetGame() {
     setSnake(initialSnakePosition);
     setScore(0);
+    setDirection("LEFT");
+    setFood({ x: 5, y: 5 });
   }
 
   function updateGame() {
-    if (
-      snake[0].x < 0 ||
-      snake[0].x >= totalGridSize ||
-      snake[0].y < 0 ||
-      snake[0].y >= totalGridSize
-    ) {
-      gameOver();
-      return;
-    }
-
-    let isBit = snake
-      .slice(1)
-      .some((ele) => ele.x === snake[0].x && ele.y === snake[0].y);
-    if (isBit) {
-      gameOver();
-      return;
-    }
-
     let newSnake = [...snake];
+    let head = { ...newSnake[0] };
 
     switch (direction) {
       case "LEFT":
-        newSnake.unshift({ x: newSnake[0].x, y: newSnake[0].y - 1 });
+        head.y -= 1;
         break;
       case "RIGHT":
-        newSnake.unshift({ x: newSnake[0].x, y: newSnake[0].y + 1 });
+        head.y += 1;
         break;
       case "UP":
-        newSnake.unshift({ x: newSnake[0].x - 1, y: newSnake[0].y });
+        head.x -= 1;
         break;
       case "DOWN":
-        newSnake.unshift({ x: newSnake[0].x + 1, y: newSnake[0].y });
+        head.x += 1;
         break;
     }
-    let isAteFood = newSnake[0].x === food.x && newSnake[0].y === food.y;
-    if (isAteFood) {
+
+    // Wrap around logic
+    head.x = (head.x + totalGridSize) % totalGridSize;
+    head.y = (head.y + totalGridSize) % totalGridSize;
+
+    newSnake.unshift(head);
+
+    // Check self-collision
+    if (newSnake.slice(1).some((ele) => ele.x === head.x && ele.y === head.y)) {
+      resetGame();
+      return;
+    }
+
+    // Eat food
+    if (head.x === food.x && head.y === food.y) {
       setScore((prev) => prev + 1);
       eatFood();
     } else {
@@ -102,7 +90,6 @@ const SnakeBoard = () => {
 
   function updateDirection(e) {
     let code = e.code;
-
     switch (code) {
       case "ArrowUp":
         if (direction !== "DOWN") setDirection("UP");
@@ -118,14 +105,18 @@ const SnakeBoard = () => {
         break;
     }
   }
+
   useEffect(() => {
     document.addEventListener("keydown", updateDirection);
     return () => document.removeEventListener("keydown", updateDirection);
   }, [direction]);
 
   function eatFood() {
-    let xPosition = Math.floor(Math.random() * totalGridSize);
-    let yPosition = Math.floor(Math.random() * totalGridSize);
+    let xPosition, yPosition;
+    do {
+      xPosition = Math.floor(Math.random() * totalGridSize);
+      yPosition = Math.floor(Math.random() * totalGridSize);
+    } while (snake.some((seg) => seg.x === xPosition && seg.y === yPosition)); // avoid placing on snake
 
     setFood({ x: xPosition, y: yPosition });
   }
@@ -135,7 +126,10 @@ const SnakeBoard = () => {
       <div className="score" alt="score">
         Score: <span>{score}</span>
       </div>
-      <div className="board">{cells}</div>
+      <button className="reset-button" onClick={resetGame}>
+        Reset Game
+      </button>
+      <div className="board">{Board()}</div>
     </div>
   );
 };
